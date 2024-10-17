97bf5175f45f59023b356abe0fca07a9f3948bab
Ben Stoica <bstoica@rpmmoves.com>
2024-10-17 11:52:39 -0400

    moved tests to folder

--- src/use-fetch/__tests__/use-fetch.test.ts
+++ src/use-fetch/__tests__/use-fetch.test.ts
@@ -1,9 +1,9 @@
 import type { Mock } from "vitest";
-import type { UseFetchOptions, UseFetchReturn } from "./use-fetch";
+import type { UseFetchOptions, UseFetchReturn } from "../use-fetch";
 import { act, renderHook, waitFor } from "@testing-library/react";
 
 import { beforeEach, describe, expect, test, vi } from "vitest";
-import useFetch from "./use-fetch";
+import useFetch from "../use-fetch";
 
 const mocks = {
   get fetch(): Mock {
